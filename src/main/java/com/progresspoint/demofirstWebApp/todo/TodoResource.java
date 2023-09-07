9c47b2ad56b49b27e459272dddc9d8f5e803366d
mihuwis <mihuwis@gmail.com>
2023-09-07 12:32:37 +0200

    Changes jrom jsp to rest-api

--- src/main/java/com/progresspoint/demofirstWebApp/todo/TodoResource.java
+++ src/main/java/com/progresspoint/demofirstWebApp/todo/TodoResource.java
@@ -1,7 +1,21 @@
 package com.progresspoint.demofirstWebApp.todo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TodoResource {
+    private ToDoService toDoService;
+
+    public TodoResource(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String username){
+        return toDoService.findByUserName(username);
+    }
 }
